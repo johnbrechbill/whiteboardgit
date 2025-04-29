5f8a7b0136b4105b5cd8c8b0d93c7876291a72a1
johnbrechbill <johnbrechbill@gmail.com>
2025-04-28 20:41:10 -0400

    Update simpleBlink.py

--- simpleBlink.py
+++ simpleBlink.py
@@ -12,20 +12,21 @@ import board
 import neopixel
 
 pixel_pin = board.D18  # GPIO 18 (physical pin 12)
-num_pixels = 9  # One LED
+num_pixels = 9  # Nine LED's
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels)
 
 # Function to set pixel brightness
 def set_brightness(brightness):
     # Set the pixel color (in RGB format). You can change the color if desired.
-    pixels[0] = (int(brightness), int(brightness), int(brightness))
+    color = (int(brightness), int(brightness), int(brightness))
+    pixels.fill(color)
 
 # Turn on the LED (white)
-pixels[0] = (0, 0, 255)
+pixels.fill = (0, 0, 255)
 time.sleep(.5)  # Keep it on for 1 second
 
 # Turn off the LED
-pixels[0] = (0, 0, 0)
+pixels.fill = (0, 0, 0)
 time.sleep(.5)  # Keep it off for 1 second
 
 # Clear all pixels (turn off all LEDs)
