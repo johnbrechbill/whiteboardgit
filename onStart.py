89273b18fb3de865bd6800fac22bb3245a50dbde
johnbrechbill <johnbrechbill@gmail.com>
2025-05-04 13:18:40 -0400

    Update onStart.py

--- onStart.py
+++ onStart.py
@@ -23,17 +23,10 @@ pixels = neopixel.NeoPixel(pixel_pin, num_pixels)
 # GPIO setup
 BUTTON_PIN = 23  # Pin 23 for the button
 
-scripts=["/home/johnbrechbill/whiteboardgit/WhiteboardTest.py","/home/johnbrechbill/whiteboardgit/simpleBlink.py","/home/johnbrechbill/whiteboardgit/simplePulse.py"]
+test_script=["/home/johnbrechbill/whiteboardgit/WhiteboardTest3.py"]
 
 GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Set pin 23 as input with pull-up resistor
-
-# Function to run the whiteboard program
-def run_upload():
-    try:
-        subprocess.run(["python3", "/home/johnbrechbill/whiteboardgit/WhiteboardTest.py"])
-    except Exception as e:
-        print(f"Error running the WhiteboardTest program: {e}")
 
 
 # Test Function To Run Script
@@ -44,42 +37,18 @@ def run_script(script_name):
     except subprocess.CalledProcessError as e:
         return f"{script_name} failed with return code {e.returncode}:\n{e.stderr}"
 
-
-# Function to run the simpleBlink program
-def run_blink():
-    try:
-        # Use Popen instead of run to prevent blocking
-        subprocess.Popen(["python3", "/home/johnbrechbill/whiteboardgit/simpleBlink.py"])
-    except Exception as e:
-        print(f"Error running the simpleBlink program: {e}")
-
 print("Waiting for button press...")
-
-def run_pulse():
-    try:
-        # Use Popen instead of run to prevent blocking
-        subprocess.Popen(["python3", "/home/johnbrechbill/whiteboardgit/simplePulse.py"])
-    except Exception as e:
-        print(f"Error running the simplePulse program: {e}")
 
 try:
     while True:
         # Wait for the button press (button will pull the pin to LOW when pressed)
         if GPIO.input(BUTTON_PIN) == GPIO.LOW:
             print("Button Pressed")
-            #run_pulse()
-            #run_upload()  # Run the WhiteboardTest.py script
-            #run_blink()
-            with concurrent.futures.ProcessPoolExecutor() as executor:
-                futures = [executor.submit(run_script, script) for script in scripts]
-                for future in concurrent.futures.as_completed(futures):
-                    print(future.result())
+            run_script(test_script)
             time.sleep(0.2)  # Debounce delay to prevent multiple detections
             # Continue looping and checking for the next button press
             time.sleep(0.1)  # Small delay to prevent high CPU usage in the loop
-
 except KeyboardInterrupt:
     print("Program stopped")
-
 finally:
     GPIO.cleanup()  # Clean up GPIO settings on exit
