044fcbc0fe7e22cc4c41039aa497e6f6f220309e
johnbrechbill <johnbrechbill@gmail.com>
2025-05-07 20:36:23 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+sys.path.append('/usr/lib/python3/dist-packages')  # Add system-wide packages path
+sys.path.append('/home/johnbrechbill/whiteboard/lib/python3.11/site-packages')
+
 import board
 
 # NeoPixel
